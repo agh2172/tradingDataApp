e637278c79766027215a2a6d9caa9c8865dbddd4
Axel <agh2172@columbia.edu>
2024-10-01 10:22:21 +0100

    Inital project

---
+++ sol-usdt-app/src/main/java/com/example/sol_usdt_app/SolUsdtAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sol_usdt_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolUsdtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolUsdtAppApplication.class, args);
+	}
+
+}
